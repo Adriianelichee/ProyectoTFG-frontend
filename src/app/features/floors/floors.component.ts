921d9490c7c5561cd57b7c072edfa324efdc4e26
Adrian <treniyt@gmail.com>
2025-05-31 20:30:16 +0200

    Tercer  commit

---
+++ src/app/features/floors/floors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floors.component',
+  imports: [],
+  templateUrl: './floors.component.html',
+  styleUrl: './floors.component.css'
+})
+export class FloorsComponent {
+
+}
